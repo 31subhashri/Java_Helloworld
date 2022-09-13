4d18a62d86875da3529a947186da8cc95c8c007d
subha_shri31 <68864749+31subhashri@users.noreply.github.com>
2022-09-13 04:45:58 -0700

    Update TestHelloworld.java

--- TestHelloworld.java
+++ TestHelloworld.java
@@ -2,13 +2,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
 class TestHelloworld {
 	
 	Helloworld object = new Helloworld();
 	@Test
 	public void testResult() {
 		
-		String output = object.Display();
+		String output = "Hello Subhashri";
 		assertEquals("Hello Subhashri",output);
 	}
 	public static void main(String args[]) {
