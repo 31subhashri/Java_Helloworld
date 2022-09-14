4143e346e0508e857a92bd626e214266d1ed3b78
subha_shri31 <68864749+31subhashri@users.noreply.github.com>
2022-09-14 10:06:11 -0700

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,15 +1,17 @@
+import java.io.*;
+import java.util.*;
 public class Helloworld 
 {
 	public String Display() {
-		String str = "Hello Subhashri";
-		return str;
+		String name = "Hello Subhashri";
+		return name;
 	}
 	
 		public static void main(String args[]) 
 		{
-			Helloworld obj = new Helloworld();
-		  String str1 = obj.Display();
-		  System.out.println(str1);
+		  Helloworld obj = new Helloworld();
+		  String name1 = obj.Display();
+		  System.out.println(name1);
 		   
 		}
 	
