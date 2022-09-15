e347775328f03abf33dae42de9cbda577cff8e01
subha_shri31 <68864749+31subhashri@users.noreply.github.com>
2022-09-15 01:26:01 -0700

    Create  Testhelloworld.java

---
+++ Testhelloworld.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class Testhelloworld extends helloworld {
+	
+	
+	@Test
+	public void testResult() {
+		helloworld ob = new helloworld();
+		String output = ob.Result();
+		assertEquals("Hello, World",output);
+	}
+
+}
